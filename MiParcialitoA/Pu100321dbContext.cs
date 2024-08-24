41f0daca47f758520e6acbd472f1a63f82fd8765
Dennis Alexander Perez Urquiza <ia.pu100321@ufg.edu.sv>
2024-08-24 14:18:38 -0600

    Agregar archivos de proyecto.

---
+++ MiParcialitoA/Pu100321dbContext.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace MiParcialitoA;
+
+public partial class Pu100321dbContext : DbContext
+{
+    public Pu100321dbContext()
+    {
+    }
+
+    public Pu100321dbContext(DbContextOptions<Pu100321dbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Departamento> Departamentos { get; set; }
+
+    public virtual DbSet<Municipio> Municipios { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=CC08-15;Database=PU100321DB;TrustServerCertificate=True;User Id=sa;Password=pu100321;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.UseCollation("SQL_Latin1_General_CP1_CI_AS");
+
+        modelBuilder.Entity<Departamento>(entity =>
+        {
+            entity.HasNoKey();
+
+            entity.Property(e => e.DepaId)
+                .HasMaxLength(255)
+                .HasColumnName("depa_id");
+            entity.Property(e => e.DepaNombre)
+                .HasMaxLength(255)
+                .HasColumnName("depa_nombre");
+        });
+
+        modelBuilder.Entity<Municipio>(entity =>
+        {
+            entity.HasNoKey();
+
+            entity.Property(e => e.DepaId)
+                .HasMaxLength(255)
+                .HasColumnName("depa_id");
+            entity.Property(e => e.MuniId)
+                .HasMaxLength(255)
+                .HasColumnName("muni_id");
+            entity.Property(e => e.MuniNombre)
+                .HasMaxLength(255)
+                .HasColumnName("muni_nombre");
+            entity.Property(e => e.MunidepaId).HasColumnName("munidepa_id");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
